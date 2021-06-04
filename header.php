b1ca39d80fa4310cd3718afee796a44cb6df07f8
arsua-greg <gregarsua@bpoc.co.jp>
2021-06-04 14:40:27 +0800

    wordpress

--- header.php
+++ header.php
@@ -6,19 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NA</title>
-    <link rel="shortcut icon" type="image/jpg" href="assets/img/NA.png">
+    <link rel="shortcut icon" type="image/jpg" href="<?php echo get_template_directory_uri(); ?>/assets/img/NA.png">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/top.css">
-    <link rel="stylesheet" href="assets/css/service.css">
-    <link rel="stylesheet" href="assets/css/flow.css">
-    <link rel="stylesheet" href="assets/css/company_info.css">
-    <link rel="stylesheet" href="assets/css/recruitment.css">
-    <link rel="stylesheet" href="assets/css/contact.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/header.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/footer.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/top.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/service.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/flow.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/company_info.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/recruitment.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/contact.css">
 </head>
 
 <body>
-    
